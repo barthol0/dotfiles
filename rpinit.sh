f360f8b6e493915be0922f7fc86f7002b5662274
Bart Bialek <bart.bialek@protonmail.com>
2020-04-25 11:31:00 +0100

    Init. Rapsberry Pi init.

---
+++ rpinit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PATH_TO_BOOT="/run/media/bartholo/boot/"
+FILE="${PATH_TO_BOOT}wpa_supplicant.conf"
+
+cd $PATH_TO_BOOT
+
+# enable UART
+printf "\nenable_uart=1" >> config.txt
+# enable SSH
+touch "${PATH_TO_BOOT}ssh"
+
+if test -f "$FILE"; then
+    echo "#################### $FILE exist"
+else
+    printf "country=UK
+    ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+    update_config=1
+    network={
+        ssid=\"TALKTALK0CA4AD\"
+        psk=\"6FY983UF\"
+        key_mgmt=WPA-PSK
+    }" >> wpa_supplicant.conf
+fi
