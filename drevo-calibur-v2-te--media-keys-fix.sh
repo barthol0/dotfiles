1d5e2c41d847140d5f7cd6a0d9a22be648036d50
Bart <bart.bialek@protonmail.com>
2022-07-31 20:57:09 +0100

    Add media keys fix for Drevo Calibur V2 TE keyboard

---
+++ drevo-calibur-v2-te--media-keys-fix.sh
@@ -0,0 +1,25 @@
+# For current session
+
+echo 0 | sudo tee /sys/module/hid_apple/parameters/fnmode
+
+# For after reboot
+
+echo options hid_apple fnmode=0 | sudo tee -a /etc/modprobe.d/hid_apple.conf
+
+if command -v mkinitcpio &> /dev/null
+
+then
+    
+    sudo mkinitcpio -P
+    
+elif command -v update-initramfs &> /dev/null
+
+then
+    
+    sudo update-initramfs -u
+    
+else
+    
+    echo "Could not find mkinitcpio or update-initramfs. Please a find an alternative for your distribution."
+    
+fi
