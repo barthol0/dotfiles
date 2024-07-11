0c3362892fdc3eedc2337fc9c0ef75e0ac322856
Bart Bialek <bart.bialek@protonmail.com>
2024-07-11 21:27:46 +0100

    Update marketplace script

--- scripts/vscodium-marketplace.py
+++ scripts/vscodium-marketplace.py
@@ -1,13 +1,25 @@
 import json
 import os
 
+# Installed via:
 
-# VSCodiumProductPath = "/usr/share/vscodium-bin/resources/app/product.json"
-VSCodiumProductPath = "/opt/vscodium-bin/resources/app/product.json"
+# Package manager - i.e. apt
+VSCodiumProductPath = "/usr/share/codium/resources/app/product.json"
+
+# Flatpak:
+# VSCodiumProductPath = "~/.local/share/flatpak/app/com.vscodium.codium-insiders/x86_64/stable/1234567890/files/share/codium-insiders/resources/app/product.json"
+# VSCodiumProductPath = "~/.local/share/flatpak/app/com.vscodium.codium/x86_64/stable/1234567890/files/share/codium/resources/app/product.json"
+
+# Find product.json file
+# sudo find ~/ -name product.json
+# sudo find / -name product.json
+
 service_url = "https://marketplace.visualstudio.com/_apis/public/gallery"
 item_url = "https://marketplace.visualstudio.com/items"
 
-if not os.path.exists(VSCodiumProductPath) or not os.access(VSCodiumProductPath, os.W_OK):
+if not os.path.exists(VSCodiumProductPath) or not os.access(
+    VSCodiumProductPath, os.W_OK
+):
     print(
         "ERROR: Insuficient permissions or did not find VSCode Product file:",
         VSCodiumProductPath,
